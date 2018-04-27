f0a496381beda088c86c72dd0113b38b4d837170
zhulan <zhulan@10.211.55.4>
2018-04-27 16:57:56 +0800

    Spring Cloud中的API网关服务Zuul
    Test OK.
    
    <artifactId>spring-boot-starter-parent</artifactId>
    <version>1.5.6.RELEASE</version>
    各个程序的版本号一定要一致。

--- api-gateway/src/main/java/com/example/demo/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/example/demo/ApiGatewayApplication.java
@@ -3,11 +3,16 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableZuulProxy
 public class ApiGatewayApplication {
 
+	@Bean
+	PermisFilter permisFilter() {
+		return new PermisFilter();
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(ApiGatewayApplication.class, args);
 	}
